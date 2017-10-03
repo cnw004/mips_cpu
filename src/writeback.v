eb594b47b90819e90caa7d6ca472efe4e032663a
Cole Whitley <colewhit29@gmail.com>
2017-10-03 19:08:33 -0400

    writeback should be good

---
+++ src/writeback.v
@@ -0,0 +1,31 @@
+`include "mux.v"
+/*
+ This module wires together everything in the fetch stage.
+
+ inputs:
+   - MemToRegW: 1 bit decides whether ALUout or read data gets passed to ResultW
+   - ReadDataW: data read from data_memory
+   - ALUOutW: value output from the ALU
+   - WriteRegW: register to be written to, passed to the hazard unit
+
+ outputs:
+   - WriteRegW_out: passing the reg into the hazard unit
+   - ResultW: value to be passed and written into the register file
+
+ modules included:
+   mux.v
+ */
+module fetch(
+   input wire MemToRegW,
+   input wire [31:0] ReadDataW,
+   input wire [31:0] ALUOutW,
+   input wire [4:0] WriteRegW,
+   output wire [4:0] WriteRegW_out,
+   output wire [31:0] ResultW
+   );
+
+   mux my_mux(MemToRegW, ALUOutW, ReadDataW, ResultW);
+
+   assign WriteRegW_out = WriteRegW;
+
+endmodule
