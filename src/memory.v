d2668ff31f8342a0350ddf8c40c23c6150dbc29a
cnw004 <cnw004@bucknell.edu>
2017-10-04 19:03:35 -0400

    added support for syscall

--- src/memory.v
+++ src/memory.v
@@ -3,6 +3,7 @@
  This module wires together everything in the memory stage.
 
  inputs:
+   - syscall: 1 bit from control, is it a syscall?
    - RegWriteM: 1 bit from control, do we reg write?
    - MemToRegM: 1 bit from control, do we pull from mem and put in reg?
    - MemWriteM: 1 bit from control, do we write to mem?
@@ -11,21 +12,25 @@
    - WriteRegM: 5 bit, reg to write to
 
  outputs:
+  - syscall_out: pass syscall bit to write module
   - output wire RegWriteW: passing regWrite into the W stage
   - MemtoRegM_out: passing MemToReg to the W stage
   - RD: output from data memory, data read
   - WriteRegM_out: reg to be passed into hazard unit
 
+
  modules included:
    data_memory.v
  */
 module memory(
+  input wire syscall,
   input wire RegWriteM,
   input wire MemToRegM,
   input wire MemWriteM,
   input wire [31:0] ALUOutM,
   input wire [31:0] WriteDataM,
   input wire [4:0] WriteRegM,
+  output wire syscall_out,
   output wire RegWriteW,
   output wire MemtoRegM_out,
   output wire [31:0] RD,
@@ -37,6 +42,7 @@ module memory(
   assign WriteRegM = WriteRegM_out;
   assign ALUOutW = ALUOutM;
   assign MemToRegM = MemtoRegM_out;
+  assign syscall_out = syscall;
 
 
 endmodule
