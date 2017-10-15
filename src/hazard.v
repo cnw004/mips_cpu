07750dcbf839ff918037e2fab0d79eaea20fceea
Cole Whitley <colewhit29@gmail.com>
2017-10-15 17:04:24 -0400

    mem to mem forwarding should work now, may have timing issue, however

--- src/hazard.v
+++ src/hazard.v
@@ -36,7 +36,7 @@ module hazard(
   input wire [4:0] RtD,
   input wire [4:0] RsE,
   input wire [4:0] RtE,
-  input wire [4:0] RsM,
+  input wire [4:0] RtM,
   input wire MemToRegE,
   input wire RegWriteE,
   input wire [4:0] WriteRegE,
@@ -110,7 +110,7 @@ always @(*) begin
   FlushE <= (branchStall || lwStall);
 
   //handle M->M forwards
-  ForwardMM <= ((RsM != `zero) && (RsM == WriteRegW) && RegWriteW && MemWriteM);
+  ForwardMM <= ((RtM != `zero) && (RtM == WriteRegW) && RegWriteW && MemWriteM);
 
 
 
