dccfc0d08f4af9114b3bf9f725eb76569fc15203
Cole Whitley <colewhit29@gmail.com>
2017-10-17 13:34:03 -0400

    updated puts

--- helloworld/puts.s
+++ helloworld/puts.s
@@ -1,12 +1,23 @@
 .text
 .global puts
- 
-# puts uses syscall 4, $a0 is the address of a 
+
+# puts uses syscall 4, $a0 is the address of a
 # null terminated string.
 # the cpu simulator will have to trap this and
 # generate a $display call
- 
+
 puts:
     li $v0, 4
-    syscall
-    jr $ra
+    j loop
+
+loop:
+  lw $a1, 0($a0)
+  addi $a0 $a0, 4
+  beq $a1, $zero, exit
+  syscall
+
+
+
+exit:
+  li $v0, 10
+  syscall
